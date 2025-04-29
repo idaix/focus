a3f32aa113dfd56d6250a28bcef61cd5410ff527
idaix <daichekkal@gmail.com>
2025-04-29 19:07:38 +0100

    TEST deploy to netlify

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,4 @@
 import { Outlet, createRootRouteWithContext } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-
-import TanstackQueryLayout from '../integrations/tanstack-query/layout'
 
 import type { QueryClient } from '@tanstack/react-query'
 
