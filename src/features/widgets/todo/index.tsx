29c08ecf2578afaca31e331e23b3a72e89aa4143
idaix <daichekkal@gmail.com>
2025-05-12 19:13:53 +0100

    add scrollbar for todos list

--- src/features/widgets/todo/index.tsx
+++ src/features/widgets/todo/index.tsx
@@ -2,25 +2,26 @@ import { useLiveQuery } from 'dexie-react-hooks'
 import { db } from './db'
 import TodoForm from './components/todo-form'
 import TodoCard from './components/todo-card'
+import { ScrollArea } from '@/components/ui/scroll-area'
 export default function TodoWidget() {
   const todos = useLiveQuery(() => db.todos.toArray())
 
   return (
-    <div className="grid grid-rows-[auto_1fr] h-full gap-3">
+    <div className="flex flex-col gap-3 h-full">
       <TodoForm />
-      <div className="h-full overflow-auto">
-        {todos?.length === 0 && (
+      <div className="flex-1 overflow-hidden">
+        {!todos || todos.length === 0 ? (
           <div className="grid place-items-center h-full text-muted-foreground">
             Add a To-do
           </div>
-        )}
-
-        {todos && todos.length > 0 && (
-          <div className="flex flex-col-reverse gap-1">
-            {todos.map((todo) => (
-              <TodoCard key={todo.id} todo={todo} />
-            ))}
-          </div>
+        ) : (
+          <ScrollArea className="h-full">
+            <div className="flex flex-col-reverse gap-1 pb-2 pr-2">
+              {todos.map((todo) => (
+                <TodoCard key={todo.id} todo={todo} />
+              ))}
+            </div>
+          </ScrollArea>
         )}
       </div>
     </div>
